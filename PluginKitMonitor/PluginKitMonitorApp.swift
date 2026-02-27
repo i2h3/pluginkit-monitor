dca6ebca21a2f0b74276381d267907b09334534e
Iva Horn <iva.horn@nextcloud.com>
2026-02-27 17:07:22 +0100

    Initial Commit

---
+++ PluginKitMonitor/PluginKitMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PluginKitMonitorApp.swift
+//  PluginKitMonitor
+//
+//  Created by Iva Horn on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct PluginKitMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PluginKitMonitorTests/PluginKitMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PluginKitMonitorTests.swift
+//  PluginKitMonitorTests
+//
+//  Created by Iva Horn on 27.02.26.
+//
+
+import Testing
+@testable import PluginKitMonitor
+
+struct PluginKitMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
